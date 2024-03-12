0734a070d111251be1b492ebfb46242e8226191c
AmirParvini <amirparvini009@gmail.com>
2024-03-12 17:26:27 +0330

    initialpop_normal

--- genetic.py
+++ genetic.py
@@ -6,6 +6,7 @@ import random as rn
 from itertools import chain
 import math
 
+NumOfVehicles = 0
 Q = 0
 NumOfDepots = 0
 NumOfCust = 0
@@ -22,13 +23,14 @@ Customers = []
 Demands = []
 
 # خواندن فایل   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-with open("F:\\pr07", "r", encoding="utf-8") as file:
+with open("F:\\MDVRP_DATA_new\pr03", "r", encoding="utf-8") as file:
     lines = file.readlines()
 
 for i, line in enumerate(lines, start=1):
     if i == 1:
         numbers = line.split()
         numbers_array = [int(num) for num in numbers]
+        NumOfVehicles = numbers_array[1]
         NumOfCust = numbers_array[2]
         NumOfDepots = numbers_array[3]
     elif i == 2:
@@ -52,7 +54,7 @@ for i, line in enumerate(lines, start=1):
         yDepots.append(numbers_array[2])
         y_Nodes.append(numbers_array[2])
 
-print('custs =', NumOfCust, '\n', 'Depots =', NumOfDepots, '\n', 'Q =', Q)
+print('custs =', NumOfCust, '\n', 'Depots =', NumOfDepots, '\n', 'vehicles of each Depot =',NumOfVehicles, '\n', 'Q =', Q, '\n', 'sumdemands =', sum(Demands))
 Custs_index = list(range(1,NumOfCust+1))
 Depots_index = list(range(Custs_index[-1]+1,NumOfCust+NumOfDepots+1))
 Nodes_Index = list(range(1,NumOfCust+NumOfDepots+1))
@@ -67,8 +69,8 @@ def DistMatrix():
     return Dist_Matrix
 # pprint.pprint(DistMatrix())
 
-print(DistMatrix().shape)
-print('custindex = ', Custs_index, '\n', 'DepotIndex = ', Depots_index)
+print('shap of distmatrix = ',DistMatrix().shape, '\n\n')
+print('custindex = ', Custs_index, '\n\n', 'DepotIndex = ', Depots_index, '\n\n')
 
 
 def Grouping(numdepots, distmatrix: DistMatrix):
@@ -82,8 +84,31 @@ def Grouping(numdepots, distmatrix: DistMatrix):
             if l == min_index:
                 GD[l].append(i)
     return GD
+print('Grouping: ',Grouping(NumOfDepots,DistMatrix()), '\n\n')
 
-print(Grouping(NumOfDepots,DistMatrix()))
+
+
+
+
+
+Chromosoms = []
+# initialpopulation -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+def Initialpopulation(paulation_num: int):
+    for i in range(paulation_num):
+        Chromosom = []
+        Selectable = Custs_index.copy()
+        while len(Selectable) > 0:
+            j = rn.choice(Selectable)
+            Chromosom.append(j)
+            Selectable.remove(j)
+        Chromosoms.append(Chromosom)
+    return Chromosoms
+# initialpopulation -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+print('initialpop =', Initialpopulation(100), '\n\n')
+
+
+
+
 
 
 plt.scatter(x_Cust, y_Cust, s=5)
@@ -93,5 +118,3 @@ for i, txt in enumerate(yDepots):
 for i, txt in enumerate(y_Cust):
     plt.text(x_Cust[i], y_Cust[i], i+1,fontsize=6, ha='right', va='bottom')
 plt.show()
-
-
